f3838075b20a700966870067787dac4075c5f8b8
Raul Duarte <raulduarte94@hotmail.com>
2022-03-30 14:18:24 -0300

    :sparkles: fixed sidebar menu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app>
+    <Navbar />
     <v-main>
-      <Navbar />
+      <SideMenu />
       <v-container fluid>
         <router-view />
       </v-container>
@@ -11,11 +12,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Navbar from '@/components/layout/Navbar.vue'
+import SideMenu from '@/components/layout/SideMenu.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    SideMenu
   },
   data: () => ({})
 })
